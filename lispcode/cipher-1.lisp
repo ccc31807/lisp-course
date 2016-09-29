54294115c6d5feb1b6e714bca0e84c9fa84f645f
Charles Carter <ccc31807@yahoo.com>
2016-09-29 13:48:55 -0400

    updates to week 5 and 6

--- lispcode/cipher-1.lisp
+++ lispcode/cipher-1.lisp
@@ -1,5 +1,6 @@
 ;; cipher-1.lisp
 
+;;;;;;;;;;;;encryption;;;;;;;;;;;;;;;;
 (defun encrypt ()
   (let ((text (coerce (read-line (format t "Enter a line of text to be encrypted: ")) 'list))
         (key (- (char-code (char-upcase (read-char (format t "Enter one character as a key: ")))) 64)))
@@ -11,18 +12,23 @@
     (format t "cy-text is [~a]~%" cy-text)
     (coerce (mapcar #'code-char cy-text) 'string)))
 
-(defun substitute-cyph (cs k)
-  (format t "in substitute-cyph: list is [~a] and key is [~a]~%" cs k)
-  (mapcar (lambda (ch) (if (> (+ k ch) 90) (- (+ k ch) 26) (+ k ch))) cs))
+(defun substitute-cyph (pt k)
+  (format t "in substitute-cyph: list is [~a] and key is [~a]~%" pt k)
+  (mapcar (lambda (ch) (if (> (+ k ch) 90) (- (+ k ch) 26) (+ k ch))) pt))
 
+;;;;;;;;;;;;encryption;;;;;;;;;;;;;;;;
 (defun decrypt ()
   (let ((text (coerce (read-line (format t "Enter a line of text to be decrypted: ")) 'list))
         (key (- (char-code (char-upcase (read-char (format t "Enter one character as a key: ")))) 64)))
     (format t "decrypt, text is [~a] and key is [~a]~%" text key)
-    (substitute-plain text key)))
+    (get-plain text key)))
 
-(defun substitute-plain (text key)
-  (cond
-    ((null text) nil)
-    (t (let ((ch (char-code (car text))))
-         (cons (if (> (+ ch key) 90) (- (- key ch) 26) (- ch key)) (substitute-plain (cdr text) key))))))
+(defun get-plain (obscure key)
+  (let* ((purified-code (mapcar #'char-code (mapcar #'char-upcase (remove-if-not #'alpha-char-p obscure))))
+         (plain-text (substitute-plain purified-code key)))
+  (format t "in get-plain, text is [~a] and key is [~a]~%" purified-code key)
+  (coerce (mapcar #'code-char plain-text) 'string)))
+
+(defun substitute-plain (pt k)
+  (format t "in substitute-plain list is [~a] and key is [~a]~%" pt k)
+  (mapcar (lambda (ch) (if (< (- ch k) 65) (+ (- ch k) 26) (- ch k))) pt))
